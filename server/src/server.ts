bf092faf6fdba1d27077955ff0b89223241e66bf
Philip Bulley <philip@milkisevil.com>
2020-02-01 18:54:56 +0000

    Add DB query on server startup; Run migrations+seeds on startup in test env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
 
+const knex = require('knex')(
+  require('./db/knexfile')[process?.env?.NODE_ENV ?? 'development']
+);
+
 async function server() {
   const context = {};
 
@@ -11,8 +15,24 @@ async function server() {
     playground: true
   });
 
+  if (process.env.NODE_ENV === 'test') {
+    console.info('🗄 Test mode: Running migrations');
+    await knex.migrate.latest();
+    console.info('🗄 Test mode: Running seeds');
+    await knex.seed.run();
+  }
+
+  const lists = await knex('lists')
+    .select('*')
+    .catch((e: any) => console.error(e));
+  console.log('🗄 Found lists?:', lists?.length);
+  const items = await knex('items')
+    .select('*')
+    .catch((e: any) => console.error(e));
+  console.info('🗄 Found items?:', items?.length);
+
   const { url } = await server.listen();
-  console.log(`🚀  Server ready at ${url}`);
+  console.info(`🚀  Server ready at ${url}`);
 }
 
 export default server;
