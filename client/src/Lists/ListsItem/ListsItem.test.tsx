740ffe53ab9afcb7fc00142dc792b22c9725f068
Philip Bulley <philip@milkisevil.com>
2020-02-08 23:38:06 +0000

    ListsItem navigates to a specific list when clicked

--- client/src/Lists/ListsItem/ListsItem.test.tsx
+++ client/src/Lists/ListsItem/ListsItem.test.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { SHOPPING_LIST } from './ListsItem.fixture';
 import ListsItem from './ListsItem';
 import { advanceTo, clear } from 'jest-date-mock';
+import { useHistory } from 'react-router-dom';
 
 // Mock icon so we don't pollute snapshot with its SVG implementation details
 jest.mock('@material-ui/icons/Schedule', () => () => (
   <i data-testid="ScheduleIcon" />
 ));
+
+jest.mock('react-router-dom', () => ({
+  useHistory: jest.fn().mockReturnValue({
+    push: jest.fn()
+  })
+}));
 
 describe('ListItem', () => {
   beforeAll(() => {
@@ -20,6 +27,21 @@ describe('ListItem', () => {
 
   it('should render', () => {
     const { container } = render(<ListsItem list={SHOPPING_LIST} />);
-    expect(container).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should navigate to a specific todo list', () => {
+    const { container } = render(<ListsItem list={SHOPPING_LIST} />);
+
+    const { push } = useHistory();
+
+    if (!container.firstElementChild) {
+      throw new Error(`Can't find firstElementChild`);
+    }
+
+    fireEvent.click(container.firstElementChild);
+
+    expect(push).toHaveBeenCalledTimes(1);
+    expect(push).toHaveBeenCalledWith(`/lists/${SHOPPING_LIST.id}`);
   });
 });
--- client/src/Lists/ListsItem/ListsItem.tsx
+++ client/src/Lists/ListsItem/ListsItem.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent, useCallback, useMemo } from 'react';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
@@ -7,12 +7,15 @@ import { Optional } from 'utility-types';
 import { formatDistance } from 'date-fns';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import styled from 'styled-components/macro';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { useHistory } from 'react-router-dom';
 
 export type ListsItemProps = {
   list: Optional<TodoList, 'items'>;
 };
 
 const ListsItem: FunctionComponent<ListsItemProps> = ({ list }) => {
+  const { push } = useHistory();
   const updatedAt = useMemo(
     () =>
       formatDistance(new Date(list.updatedAt), new Date().getTime(), {
@@ -20,15 +23,21 @@ const ListsItem: FunctionComponent<ListsItemProps> = ({ list }) => {
       }),
     [list.updatedAt]
   );
+  const handleClick = useCallback(() => push(`/lists/${list.id}`), [
+    push,
+    list.id
+  ]);
 
   return (
-    <Card>
-      <CardContent>
-        <Name variant="h2">{list.name}</Name>
-        <ScheduleIconStyled fontSize="small" />
-        <Typography variant="caption">{updatedAt}</Typography>
-      </CardContent>
-    </Card>
+    <CardButtonBase onClick={handleClick}>
+      <Card>
+        <CardContent>
+          <Name variant="h2">{list.name}</Name>
+          <ScheduleIconStyled fontSize="small" />
+          <Typography variant="caption">{updatedAt}</Typography>
+        </CardContent>
+      </Card>
+    </CardButtonBase>
   );
 };
 
@@ -41,4 +50,11 @@ const ScheduleIconStyled = styled(ScheduleIcon)`
 
 const Name = styled(Typography).attrs({ variant: 'h2' })`
   margin-bottom: 40px;
+`;
+
+const CardButtonBase = styled(ButtonBase)`
+  display: block;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
 `;
